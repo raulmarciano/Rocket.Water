d15243120039c8a721f26920a88b8429b701974b
raulmarciano <raul_marciano@outlook.com>
2021-02-02 23:25:24 -0300

    Criação do form

---
+++ LembreteBeberAgua/LembreteBeberAgua/frmConfiguracoes.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+
+namespace LembreteBeberAgua
+{
+    public partial class frmConfiguracoes : Form
+    {
+        private bool manterAplicacaoAberta = true;
+        private readonly string caminho = @"C:\Program Files (x86)\Lembrete Beber Agua\LembreteAgua.exe";
+        private readonly string subKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+        public frmConfiguracoes()
+        {
+            InitializeComponent();
+        }
+    }
+}
