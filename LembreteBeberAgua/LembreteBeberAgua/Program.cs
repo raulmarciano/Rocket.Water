0c8d4b99a599756ffa0f63663998c5dfe66f57bb
raulmarciano <raul_marciano@outlook.com>
2021-02-02 23:23:32 -0300

    Criação do projeto

---
+++ LembreteBeberAgua/LembreteBeberAgua/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LembreteBeberAgua
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Process processoAtual = Process.GetCurrentProcess();
+
+            if (Process.GetProcessesByName(processoAtual.ProcessName).Any(p => p.Id != processoAtual.Id && !p.HasExited))
+            {
+                MessageBox.Show("O aplicativo já está em execução!", "Já está em execução");
+                Process.GetCurrentProcess().Kill();
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmConfiguracoes());
+        }
+    }
+}
