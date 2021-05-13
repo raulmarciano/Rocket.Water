4e4655d9ff97f930549f8bd13621fd439cdc89fe
raulmarciano <raul_marciano@outlook.com>
2021-05-13 12:42:24 -0300

    Remoção completa do nome "LembreteBeberAgua"

--- Rocket.Water/Properties/Resources.Designer.cs
+++ Rocket.Water/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LembreteBeberAgua.Properties {
+namespace RocketWater.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LembreteBeberAgua.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LembreteBeberAgua.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RocketWater.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
